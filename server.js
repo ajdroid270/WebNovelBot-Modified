0b5fce1dd4123e559ebf14dec6a2de57f58e4231
Ajay Bhargav Alli <milkyway0earth@gmail.com>
2021-01-11 21:43:15 +0530

    refractor: auto formatting and server port changed to 3003

--- server.js
+++ server.js
@@ -1,40 +1,44 @@
-const express = require('express');
-const log = require('./pinglog.txt');
+const express = require("express");
+const log = require("./pinglog.txt");
 const moment = require("moment");
 const server = express();
 
 const DiscordClient = {};
-server.all('/', (req, res)=>{
-  res.send('Your bot is alive!');
+server.all("/", (req, res) => {
+  res.send("Your bot is alive!");
 });
 
-server.all('/uptimerobot', (req, res)=>{
+server.all("/uptimerobot", (req, res) => {
   console.log("pinged at ", new Date());
-  if(DiscordClient.client){
+  if (DiscordClient.client) {
     DiscordClient.client.guilds.cache.forEach((guild, guildId, guildCache) => {
-    guild.channels.cache.forEach((channel, channelId, channelCache) => {
-      // console.log(channel.name);
-      if (
-        channel.name == DiscordClient.logChannel &&
-        guild.name == DiscordClient.myTestGuild
-      ) {
-        channel.send(`Got Pinged by ${req.query.client ?req.query.client:'me' } @ ${moment()}`);
-      }
+      guild.channels.cache.forEach((channel, channelId, channelCache) => {
+        // console.log(channel.name);
+        if (
+          channel.name == DiscordClient.logChannel &&
+          guild.name == DiscordClient.myTestGuild
+        ) {
+          channel.send(
+            `Got Pinged by ${
+              req.query.client ? req.query.client : "me"
+            } @ ${moment()}`
+          );
+        }
+      });
     });
-  });
   }
-  res.send('Your bot is alive!');
+  res.send("Your bot is alive!");
 });
 
-function keepAlive(client, validChannelsList, myTestGuild, logChannel){
-    server.listen(3000, ()=>{
-      console.log("Server is Ready!")
-      setTimeout(()=>{
-        DiscordClient.client = client;
-        DiscordClient.validChannelsList = validChannelsList;
-        DiscordClient.myTestGuild = myTestGuild;
-        DiscordClient.logChannel = logChannel;
-      }, 5000);
-    });
+function keepAlive(client, validChannelsList, myTestGuild, logChannel) {
+  server.listen(3003, () => {
+    console.log("Server is Ready!");
+    setTimeout(() => {
+      DiscordClient.client = client;
+      DiscordClient.validChannelsList = validChannelsList;
+      DiscordClient.myTestGuild = myTestGuild;
+      DiscordClient.logChannel = logChannel;
+    }, 5000);
+  });
 }
 module.exports = keepAlive;
