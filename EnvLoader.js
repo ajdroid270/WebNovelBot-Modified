0254e2bbd3d9d9c67db16fb5f8c61bc43727a521
Ajay Bhargav Alli <milkyway0earth@gmail.com>
2021-02-25 08:51:08 +0530

    env loader issue on replit fixed

--- EnvLoader.js
+++ EnvLoader.js
@@ -1,17 +1,27 @@
 const configVars = {};
+// try {
+//   const aws = require("aws-sdk");
+//   let s3 = new aws.S3({
+//     env: process.env,
+//   });
+//   configVars.isHeroku = true;
+//   configVars.env = s3.env;
+// } catch (error) {
+//   const dotenv = require("dotenv");
+//   dotenv.config();
+//   console.log("Platform is not Heroku!");
+//   configVars.isHeroku = false;
+//   configVars.env = process.env;
+// }
+
 try {
-  const aws = require("aws-sdk");
-  let s3 = new aws.S3({
-    env: process.env,
-  });
-  configVars.isHeroku = true;
-  configVars.env = s3.env;
-} catch (error) {
   const dotenv = require("dotenv");
   dotenv.config();
   console.log("Platform is not Heroku!");
   configVars.isHeroku = false;
   configVars.env = process.env;
+} catch (e) {
+  console.log(e);
 }
 
 module.exports = configVars;
